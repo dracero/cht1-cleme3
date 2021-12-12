e55fa8e52d88808367bb93b8be1250e3b030edac
Manuel <75450615+vlk-yr@users.noreply.github.com>
2021-12-12 20:58:45 -0300

    test GraphQL

--- components/chatbot.js
+++ components/chatbot.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
-import config from "../src/config";
-import MessageParser from "../src/MessageParser";
-import ActionProvider from "../src/ActionProvider";
+import config from "../config";
+import MessageParser from "../MessageParser";
+import ActionProvider from "../ActionProvider";
 
 function App() {
   return (
