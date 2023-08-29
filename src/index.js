61f1c57512c2a838ba8ca4435507c582812ba3d4
Diego Racero <diego.racero@gmail.com>
2023-08-29 14:53:30 -0300

    Update index.js
    cambioApollo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/chatbot';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-
+//Cambio del server de Apollo
 const client = new ApolloClient({
   uri: 'https://cetec-apollo-server.onrender.com',
   cache: new InMemoryCache(),
